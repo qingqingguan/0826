2f1214c2caa7aaaba2d9d37b66c71ff176b09f19
qingqingguan <18229918212@163.com>
2019-09-13 17:08:23 +0800

    删除 .idea
    Signed-off-by: qingqingguan <18229918212@163.com>

---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/9/13 17:08
+# @Author  : qingqingguan
+# @Email   : qingqing.guan@eeoa.com
+# @File    : test.py
+# @Software: PyCharm
+
+print('111111111111111')
