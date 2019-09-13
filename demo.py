61133bf012c60a75d5b3b461c077f5694661f0e0
qingqingguan <18229918212@163.com>
2019-09-13 14:49:53 +0800

    删除 .idea
    Signed-off-by: qingqingguan <18229918212@163.com>

---
+++ demo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/9/13 14:43
+# @Author  : qingqingguan
+# @Email   : qingqing.guan@eeoa.com
+# @File    : demo.py
+# @Software: PyCharm
+
+print('hello world')
