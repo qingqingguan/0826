8d0471d82b3d8f3196eddbf60cd5e73283d7044d
qingqingguan <18229918212@163.com>
2019-09-13 17:13:16 +0800

    add file

---
+++ pushpush.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/9/13 17:09
+# @Author  : qingqingguan
+# @Email   : qingqing.guan@eeoa.com
+# @File    : pushpush.py
+# @Software: PyCharm
+
+print('this is pushed by terminal')
